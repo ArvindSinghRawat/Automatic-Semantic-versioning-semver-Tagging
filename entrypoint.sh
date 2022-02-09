5b764f24850d0d69a12da933a26a0a9ff5ba12fb
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2022-02-09 22:20:25 +0530

    First draft for Shell File to create git tags

---
+++ entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "Starting the taging process based on commit message +semver: xxxxx"
+
+cd ${GITHUB_WORKSPACE}/${source}
+
+#get highest tags across all branches, not just the current branch
+VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+if [[ $VERSION == '' ]]
+then
+  VERSION='0.0.0'
+fi
+echo "Current Version: $VERSION"
+
+# split into array
+VERSION_BITS=(${VERSION//./ })
+
+echo "Latest version tag: $VERSION"
+
+#get number parts and increase last one by 1
+VNUM1=${VERSION_BITS[0]}
+VNUM2=${VERSION_BITS[1]}
+VNUM3=${VERSION_BITS[2]}
+
+COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR=`git log -1 --pretty=%B | egrep -ci '^(breaking)|(major)'`
+COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR=`git log -1 --pretty=%B | egrep -ci '^(feature)|minor)'`
+
+if [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR -gt 0 ]; then
+    VNUM1=$((VNUM1+1))
+    VNUM2=0
+    VNUM3=0
+elif [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR -gt 0 ]; then
+    VNUM2=$((VNUM2+1)) 
+    VNUM3=0
+else
+    VNUM3=$((VNUM3+1)) 
+fi
+
+# count all commits for a branch
+GIT_COMMIT_COUNT=`git rev-list --count HEAD`
+echo "Commit count: $GIT_COMMIT_COUNT" 
+export BUILD_NUMBER=$GIT_COMMIT_COUNT
+
+#create new tag
+NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
+
+echo "Updating $VERSION to $NEW_TAG"
+
+#get current hash and see if it already has a tag
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+
+
+#only tag if commit message have version-bump-message as mentioned above
+if [ -z "$NEEDS_TAG" ]; then
+    if [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR -gt 0 ] ||  [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR -gt 0 ] || [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_PATCH -gt 0 ]; then
+        echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
+        git tag "$NEW_TAG"
+        git push origin $NEW_TAG
+    fi
+else
+    echo "Already a tag on this commit"
+fi
