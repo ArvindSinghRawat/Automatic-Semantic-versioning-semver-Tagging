6b9de8a692711d51adc89376911830bed943919f
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2024-04-15 01:05:28 +0530

    Added directory back

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,6 +67,8 @@ function to_number {
 
 echo "Starting the tagging process based on commit message +semver: xxxxx"
 
+git config --global --add safe.directory '${GITHUB_WORKSPACE}/${source}'
+
 cd ${GITHUB_WORKSPACE}/${source}
 
 # Get current hash and see if it already has a tag
