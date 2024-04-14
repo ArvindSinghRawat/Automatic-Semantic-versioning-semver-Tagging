a57795e34f5d477d2aa6803552c27bedfdc8797a
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2024-04-14 23:01:48 +0530

    Modularized shell script - Moved relevant logic to reusable methods - Removed redundant concept of `TO_PUSH` - Avoid applying any logic, if commit already had a tag - Started using local variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,69 +1,92 @@
 #!/bin/bash
 
+function get_current_version {
+    # Get the latest tag across all branches, not just the current branch
+    local VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+    if [[ $VERSION == '' ]]
+    then
+        VERSION='0.0.0'
+    fi
+    echo "$VERSION"
+}
+
+function update_version {
+    # Updates the provided version to next version based on commit message
+    # Takes the current version in Semantic versioning format, for eg: 0.0.1 (without any prefix or suffix)
+    local CURRENT_VERSION=$1
+    local NEXT_VERSION=''
+    if [[$CURRENT_VERSION == '']]
+    then
+        NEXT_VERSION='0.0.1'
+    else
+        local VERSION_PARTS=(${CURRENT_VERSION//./ })
+        V_MAJOR=$(to_number ${VERSION_PARTS[0]})
+        V_MINOR=$(to_number ${VERSION_PARTS[1]})
+        V_PATCH=$(to_number ${VERSION_PARTS[2]})
+
+        local IS_MAJOR_CHANGE=`git log -1 --pretty=%B | egrep -ci '^(breaking)|(release)|(major)'`
+        local IS_MINOR_CHANGE=`git log -1 --pretty=%B | egrep -ci '^(feature)|(minor)'`
+
+        if [ $IS_MAJOR_CHANGE -gt 0 ]; 
+        then
+            echo "Bumping major version by 1"
+            V_MAJOR=$((V_MAJOR+1))
+            V_MINOR=0
+            V_PATCH=0
+        elif [ $IS_MINOR_CHANGE -gt 0 ];
+        then
+            echo "Bumping minor version by 1"
+            V_MINOR=$((V_MINOR+1))
+            V_PATCH=0
+        else
+            echo "Bumping patch version by 1"
+            V_PATCH=$((V_PATCH+1))
+        fi
+        NEXT_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
+
+        echo "$NEXT_VERSION"
+
+}
+
+function to_number {
+    # Checks if provided parameter is a number or not. If not, then defaults to Zero.
+    local $VALUE="$1"
+    if [[ $VALUE =~ ^[0-9]+$ ]];
+    then
+        echo "$VALUE"
+    else
+        echo "0"
+    fi
+}
+
 echo "Starting the taging process based on commit message +semver: xxxxx"
 
 git config --global --add safe.directory '*'
 
 cd ${GITHUB_WORKSPACE}/${source}
 
-#get highest tags across all branches, not just the current branch
-VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
-
-if [[ $VERSION == '' ]]
-then
-  VERSION='0.0.0'
-fi
-echo "Current Version: $VERSION"
-
-# split into array
-VERSION_BITS=(${VERSION//./ })
-
-echo "Latest version tag: $VERSION"
-
-#get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
-
-COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR=`git log -1 --pretty=%B | egrep -ci '^(breaking)|(major)'`
-COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR=`git log -1 --pretty=%B | egrep -ci '^(feature)|(minor)'`
-TO_PUSH=false
-
-if [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR -gt 0 ]; then
-    VNUM1=$((VNUM1+1))
-    VNUM2=0
-    VNUM3=0
-    TO_PUSH=true
-elif [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR -gt 0 ]; then
-    VNUM2=$((VNUM2+1)) 
-    VNUM3=0
-    TO_PUSH=true
-else
-    VNUM3=$((VNUM3+1)) 
-    TO_PUSH=true
-fi
-
-#create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-
-echo "Updating $VERSION to $NEW_TAG"
-
-#get current hash and see if it already has a tag
+# Get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 
-#only tag if commit message have version-bump-message as mentioned above
+# Only tag if commit message have version-bump-message as mentioned above
 if [ -z "$NEEDS_TAG" ]; then
-    if [ $TO_PUSH ]; then
-        echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-        git tag "$NEW_TAG"
-        # git push origin $NEW_TAG -f
-        git push origin $NEW_TAG
-        echo "Success"
-    else
-        echo "Failed"
-    fi
+    # Get latest tag across all branches, not just the current branch
+    # Ignore in case of error, generally happens if no tag is present
+    local VERSION=$(get_current_version)
+    echo "Current Version: $VERSION"
+
+    # Create a new version from existing tag
+    local NEW_VERSION=$(update_version VERSION)
+    echo "Latest version tag: $NEW_VERSION"
+
+    echo "Tagged with $NEW_VERSION (Ignoring fatal:cannot describe - this means commit is untagged) "
+    git tag "$NEW_VERSION"
+    set -e
+    git push origin $NEW_VERSION
+    echo "Success"
 else
     echo "Already a tag on this commit"
 fi
