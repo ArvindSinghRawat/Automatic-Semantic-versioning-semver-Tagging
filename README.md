c2c2e6d37c4ad5f1972720163198f1128e72c287
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2024-04-15 01:18:14 +0530

    Breaking: Refactored project and added more details into it. (#3)
    * Added more detailed descriptions and docs
    * Modularized shell script
      - Moved relevant logic to reusable methods
      - Removed redundant concept of `TO_PUSH`
      - Avoid applying any logic, if the commit already has a tag
      - Started using local variables
    * Added `.dockerignore`
    * Added `CONTRIBUTING.md`
    * Fixed Repo URL
    * Changed return values to a global variable
      - Added check, if the new tag is actually updated

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# Git-Automatic-Semver-Tagging
+# Automatic Semantic versioning(SemVer) Tagging
 
 ## Introduction
 
-Provides the semantic versioning for the Repo.
+Automatically provides the semantic versioning as Tags for a Github repository.
 Semantic versioning is like `MAJOR`.`MINOR`.`PATCH`.
 
-## Usage [![Latest Release](https://img.shields.io/github/v/release/ArvindSinghRawat/Git-Automatic-Semver-Tagging?color=%233D9970)](https://img.shields.io/github/v/release/ArvindSinghRawat/Git-Automatic-Semver-Tagging?color=%233D9970)
+Benefits of SemVer tags:
+- **Predictability**: SemVer gives devs a way to understand the impact of updating a dependency. They can tell at a glance whether an update includes major changes, new features or just bug fixes.
+- **Release or Upgrade planning**: SemVer can help users of a library or an API to understand when and how they need to update their own code to stay compatible. Eg: If their is a major change, it needs to more time and work to accomodate as opposed to a patch change.
+- **Communication**: SemVer clearly communicates what kind of changes were made in each release. It also assists in supporting backward compatibility. 
+
+
+
+## Usage [![Latest Release](https://img.shields.io/github/v/release/ArvindSinghRawat/Automatic-Semantic-versioning-semver-Tagging?color=%233D9970)](https://img.shields.io/github/v/release/ArvindSinghRawat/Automatic-Semantic-versioning-semver-Tagging?color=%233D9970)
 
 ```yml
 name: Git Auto Semver Tag
@@ -14,14 +21,20 @@ on:
   push:
     branches: [ main ]
 
+permissions:
+  contents: write
+
 jobs:
   auto-tag:
     runs-on: ubuntu-latest
     steps:
-    - uses: actions/checkout@v2
+    - uses: actions/checkout@v4
       with:
-        fetch-depth: '0'
-    - uses: ArvindSinghRawat/Git-Automatic-Semver-Tagging@main
+        fetch-depth: 0
+        fetch-tags: true
+        set-safe-directory: true
+        
+    - uses: ArvindSinghRawat/Automatic-Semantic-versioning-semver-Tagging@main
       env:
         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,69 +1,103 @@
 #!/bin/bash
 
-echo "Starting the taging process based on commit message +semver: xxxxx"
+NEW_VERSION=''
+
+# Function to get the current version based on the latest tag
+function get_current_version {
+    # Get the latest tag across all branches, not just the current branch
+    local VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+
+    if [[ $VERSION == '' ]]
+    then
+        VERSION='0.0.0'
+    fi
+    echo "$VERSION"
+}
+
+# Function to update the version based on commit message
+function update_version {
+    # Updates the provided version to the next version based on the commit message
+    # Takes the current version in Semantic versioning format, for example: 0.0.1 (without any prefix or suffix)
+    local CURRENT_VERSION=$1
+    local NEXT_VERSION=''
+    if [[ $CURRENT_VERSION == '' ]]
+    then
+        NEXT_VERSION='0.0.1'
+    else
+        local VERSION_PARTS=(${CURRENT_VERSION//./ })
+        to_number ${VERSION_PARTS[0]}
+        V_MAJOR=$?
+        to_number ${VERSION_PARTS[1]}
+        V_MINOR=$?
+        to_number ${VERSION_PARTS[2]}
+        V_PATCH=$?
+
+        local IS_MAJOR_CHANGE=`git log -1 --pretty=%B | egrep -ci '^(breaking)|(release)|(major)'`
+        local IS_MINOR_CHANGE=`git log -1 --pretty=%B | egrep -ci '^(feature)|(minor)'`
+
+        if [ $IS_MAJOR_CHANGE -gt 0 ]; 
+        then
+            V_MAJOR=$((V_MAJOR+1))
+            V_MINOR=0
+            V_PATCH=0
+        elif [ $IS_MINOR_CHANGE -gt 0 ];
+        then
+            V_MINOR=$((V_MINOR+1))
+            V_PATCH=0
+        else
+            V_PATCH=$((V_PATCH+1))
+        fi
+        NEXT_VERSION="$V_MAJOR.$V_MINOR.$V_PATCH"
+    fi
+    NEW_VERSION="$NEXT_VERSION"
+}
+
+# Function to check whether provided string is a number or not
+function to_number {
+    # Checks if provided parameter is a number or not. If not, then defaults to Zero.
+    echo "Provided value is $1"
+    local VALUE=$1
+    if [[ $VALUE =~ ^[0-9]+$ ]];
+    then
+        return $VALUE
+    else
+        return 0
+    fi
+}
+
+echo "Starting the tagging process based on commit message +semver: xxxxx"
 
 git config --global --add safe.directory '*'
 
 cd ${GITHUB_WORKSPACE}/${source}
 
-#get highest tags across all branches, not just the current branch
-VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
-
-if [[ $VERSION == '' ]]
-then
-  VERSION='0.0.0'
-fi
-echo "Current Version: $VERSION"
-
-# split into array
-VERSION_BITS=(${VERSION//./ })
-
-echo "Latest version tag: $VERSION"
-
-#get number parts and increase last one by 1
-VNUM1=${VERSION_BITS[0]}
-VNUM2=${VERSION_BITS[1]}
-VNUM3=${VERSION_BITS[2]}
-
-COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR=`git log -1 --pretty=%B | egrep -ci '^(breaking)|(major)'`
-COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR=`git log -1 --pretty=%B | egrep -ci '^(feature)|(minor)'`
-TO_PUSH=false
-
-if [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MAJOR -gt 0 ]; then
-    VNUM1=$((VNUM1+1))
-    VNUM2=0
-    VNUM3=0
-    TO_PUSH=true
-elif [ $COUNT_OF_COMMIT_MSG_HAVE_SEMVER_MINOR -gt 0 ]; then
-    VNUM2=$((VNUM2+1)) 
-    VNUM3=0
-    TO_PUSH=true
-else
-    VNUM3=$((VNUM3+1)) 
-    TO_PUSH=true
-fi
-
-#create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-
-echo "Updating $VERSION to $NEW_TAG"
-
-#get current hash and see if it already has a tag
+# Get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 
-#only tag if commit message have version-bump-message as mentioned above
+# Only tag if the commit message has the version-bump-message as mentioned in docs
 if [ -z "$NEEDS_TAG" ]; then
-    if [ $TO_PUSH ]; then
-        echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
-        git tag "$NEW_TAG"
-        # git push origin $NEW_TAG -f
-        git push origin $NEW_TAG
-        echo "Success"
-    else
-        echo "Failed"
+    # Get the latest tag across all branches, not just the current branch
+    # Ignore in case of error, generally happens if no tag is present
+    VERSION=$(get_current_version)
+    echo "Current Version: $VERSION"
+
+    # Create a new version from the existing tag
+    update_version $VERSION
+    echo "Latest version tag is: $NEW_VERSION"
+
+    if [[ $NEW_VERSION == '' ]];
+    then
+        echo "Tag is not yet set" >&2
+        exit 1
     fi
+
+    git tag "$NEW_VERSION"
+    echo "Tagged with $NEW_VERSION (Ignoring fatal:cannot describe - this means commit is untagged) "
+    set -e
+    git push origin $NEW_VERSION --verbose
+    echo "Success"
 else
     echo "Already a tag on this commit"
 fi
